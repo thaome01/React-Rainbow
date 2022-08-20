ef063c1a3131947fc60cc2f95f73b23f41010cf7
Meng Thao <mengthao023@gmail.com>
2022-08-19 19:43:44 -0500

    useState

---
+++ App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+
+function App() {
+  let [colors, setColors] = useState([
+    'violet', 'blue',
+    'lightblue', 'green',
+    'greenyellow', 'yellow',
+    'orange', 'red',
+  ])
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} />
+    )
+  })
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  return (
+    <div className="App">
+
+      {colors.map((color, i) =>
+        <ColorBlock key={i} color={color} />
+      )}
+
+      <ColorForm addColor={addColor} />
+    </div>
+  )
+
+}
+
+export default App;
